c141639726137756a37c4ec03041ced87dc221ac
Segev.Mashraki@amdocs.com <Segev.Mashraki@amdocs.com>
2025-11-06 21:12:34 +0200

    more files

--- recreate_msg.cmd
+++ recreate_msg.cmd
@@ -1,3 +1,3 @@
-python.exe .\map_files.py .\output\1000_messages_output.txt .\output\1000_messages_output_hebrew.txt .\output\1000_mapping.txt 26
-python translate_csv.py output\1000_messages.csv output\1000_mapping.txt output\1000_messages_hebrew.csv
-python.exe .\create_msg.py output\1000_messages_hebrew.csv C:\Games\KQ8\daventry\English\1000.MSG
+python.exe .\map_files.py .\daventry\1000_messages_output.txt .\daventry\1000_messages_daventry_hebrew.txt .\daventry\1000_mapping.txt 26
+python translate_csv.py daventry\1000_messages.csv daventry\1000_mapping.txt daventry\1000_messages_hebrew.csv
+python.exe .\create_msg.py daventry\1000_messages_hebrew.csv C:\Games\KQ8\daventry\English\1000.MSG
